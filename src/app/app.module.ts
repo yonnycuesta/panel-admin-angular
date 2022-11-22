f54404defd2ea2ab1d4ad135b7955cc6eaec73dc
yonnycuesta <yocumo1998@gmail.com>
2022-11-22 17:21:32 -0500

    Second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
+import { NavbarComponent } from './dashboard/navbar/navbar.component';
+import { FooterComponent } from './dashboard/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SidebarComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
